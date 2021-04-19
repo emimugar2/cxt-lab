c428e61b1001a36cda1fbee752e20210a8eba712
Julio Fernández Fernández <julio.ffernandez@udc.es>
2021-04-20 00:07:15 +0200

    Revert "Merge branch 'hotfix/9' into 'main'"
    This reverts merge request !2

--- complete/src/main/java/com/example/restservice/CitiesController.java
+++ complete/src/main/java/com/example/restservice/CitiesController.java
@@ -1,5 +1,4 @@
 package com.example.restservice;
-import com.example.*;
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,23 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CitiesController {
 
-    ArrayList<Cities> cityRepository = new ArrayList<Cities>();
-    public CitiesController(){
-        cityRepository.add(new Cities(1, "Coruña", "Coruña"));
-        cityRepository.add(new Cities(2, "Pontevedra", "Pontevedra"));
-    }
+    @Autowired
+    public List<String>  cityRepository = Arrays.asList("Coruña", "Vigo", "Ourense", "Pontevedra");
     @GetMapping("/cities")
-    public ArrayList<Cities> getCities() {
-        return cityRepository;
+    public String[] getCities(@RequestParam(value = "value", required = false) String value) {
+        List<String> cities = null;
+        cities = (List<String>) cityRepository;
+        return cities.toArray(new String[0]);
     }
     @GetMapping("/city/{id}")
-    public Cities getCityById(@PathVariable ("id") int id) {
-        for (int i=0;i<cityRepository.size(); i++) {
-            Cities ciudad = cityRepository.get(i);
-            if (ciudad.getId()==id) {
-                return ciudad;
-            }
-        }
-        return new Cities(404, "No existe", "TAbarnia");
+    public String getCityById(@PathVariable Long id) {
+        String city = cityRepository.get(id.intValue());
+        return city;
     }
 }
