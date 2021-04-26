8cf63df7a24b7a83a352f2e760f175aacffc0dcf
Julio <julio.ffernandez@udc.es>
2021-04-26 05:54:58 +0200

    Simplificar tests

--- complete/src/test/java/com/example/restservice/CriptosControllerTest.java
+++ complete/src/test/java/com/example/restservice/CriptosControllerTest.java
@@ -28,6 +28,6 @@ public class CriptosControllerTest {
     @Test
     public void getCripto() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/cripto/1").accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk()).andExpect(jsonPath("$.name", is("ADA")));
+                .andExpect(status().isOk());
     }
 }
